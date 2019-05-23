819020ff50084b09e661251de2ffb5848a1f9eef
F_uu123 <421342785@qq.com>
2019-05-23 23:20:15 +0800

    改了点配置。。。

--- src/main/java/com/redemption/hair/lowCNKI/LowCnkiApplication.java
+++ src/main/java/com/redemption/hair/lowCNKI/LowCnkiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LowCnkiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LowCnkiApplication.class, args);
 	}
