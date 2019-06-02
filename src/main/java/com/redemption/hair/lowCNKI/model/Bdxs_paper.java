e49797234fdbbaf6fdc836c82a2ddea3d49ddd16
WInjace <15811599533@163.com>
2019-06-02 14:21:51 +0800

    3 new models

---
+++ src/main/java/com/redemption/hair/lowCNKI/model/Bdxs_paper.java
@@ -0,0 +1,121 @@
+package com.redemption.hair.lowCNKI.model;
+
+public class Bdxs_paper {
+    private String paperId;
+    private String title;
+    private String sourceName;
+    private String sourceUrl;
+    private String authorName;
+    private String summary;
+    private String keywords;
+    private String doi;
+    private String cited;
+    private String downLoadUrl;
+    private String journalTitle;
+    private String journalPicture;
+    private String fields;
+
+    public String getPaperId() {
+        return paperId;
+    }
+
+    public void setPaperId(String paperId) {
+        this.paperId = paperId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSourceName() {
+        return sourceName;
+    }
+
+    public void setSourceName(String sourceName) {
+        this.sourceName = sourceName;
+    }
+
+    public String getSourceUrl() {
+        return sourceUrl;
+    }
+
+    public void setSourceUrl(String sourceUrl) {
+        this.sourceUrl = sourceUrl;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getDoi() {
+        return doi;
+    }
+
+    public void setDoi(String doi) {
+        this.doi = doi;
+    }
+
+    public String getCited() {
+        return cited;
+    }
+
+    public void setCited(String cited) {
+        this.cited = cited;
+    }
+
+    public String getDownLoadUrl() {
+        return downLoadUrl;
+    }
+
+    public void setDownLoadUrl(String downLoadUrl) {
+        this.downLoadUrl = downLoadUrl;
+    }
+
+    public String getJournalTitle() {
+        return journalTitle;
+    }
+
+    public void setJournalTitle(String journalTitle) {
+        this.journalTitle = journalTitle;
+    }
+
+    public String getJournalPicture() {
+        return journalPicture;
+    }
+
+    public void setJournalPicture(String journalPicture) {
+        this.journalPicture = journalPicture;
+    }
+
+    public String getFields() {
+        return fields;
+    }
+
+    public void setFields(String fields) {
+        this.fields = fields;
+    }
+}
